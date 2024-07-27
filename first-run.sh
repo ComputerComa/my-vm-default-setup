585535f656e4b5af9db2e6a6ef03fee2e58c7379
ComputerComa <walkerraydick2@gmail.com>
2024-07-27 12:23:48 -0500

    Update first-run.sh

--- first-run.sh
+++ first-run.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+$SUDO a_command
+
 echo "======== Pulling Updates ======="
-apt-get update
+$SUDO apt-get update
 echo "======== Installing Updates ========"
-apt-get upgrade -y
+$SUDO apt-get upgrade -y
 echo "======== Installing Packages ======="
-apt-get install zsh git language-pack-en fonts-powerline -y
+$SUDO apt-get install zsh git language-pack-en fonts-powerline -y
 
 echo "======= Setting Default Shell to ZSH ======="
 
-chsh -s $(which zsh)
+$SUDO chsh -s $(which zsh)
 
 echo $SHELL
 
 echo "======== Installing OhMyZsh ======="
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+$SUDO sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 echo "Pulling Default Config from github"
-rm ~/.zshrc
+$SUDO rm ~/.zshrc
 
 wget https://raw.githubusercontent.com/ComputerComa/my-vm-default-setup/main/.zshrc
 echo "reloading ZSH"
 zsh -ic "omz reload"
 
 echo "======= Setting Timezone ========"
-timedatectl set-timezone America/Chicago
+$SUDO timedatectl set-timezone America/Chicago
 
 
 echo "======== Script Complete ========"
